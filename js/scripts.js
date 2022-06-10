af9f6887d22d9c79b17627b22b2ffabfeefe255d
Grace Kostanich <fl5812597@gmail.com>
2022-06-10 13:23:41 -0700

    WIP: exclude previous code, attempt new methods

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,12 @@ function beepBoop(number) {
   for (let i = 0; i <= number; i++) {
       arrayRange.push(i)
   }
-  console.log(arrayRange);
-//cut previous working function
-
-let testArray = arrayRange.toString();
-let newArrayRange = testArray.map(function(element){
-    console.log(newArrayRange)
-  if (newArrayRange.includes("1","2","3")) {
-   // let wordsArrayRange = element.toString();
-    return testArray.replaceAll("ok")
-  }
-  else { 
-  return element
-  }
-
-});
-console.log(newArrayRange);
+ // console.log(arrayRange);
+ let stringRange = arrayRange.filter(
+  element => typeof element === 'string'
+ );
+ console.log(stringRange)
 }
+
   beepBoop(14);
   //
