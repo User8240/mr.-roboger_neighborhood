315fac38cf47209156c68cce7aa07fb58faf5600
Grace Kostanich <fl5812597@gmail.com>
2022-06-10 11:12:27 -0700

    WIP: Include next test to output values of 1-3 as strings, not yet executable

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,20 @@ function beepBoop(number) {
       arrayRange.push(i)
   }
   console.log(arrayRange);
+//cut previous working function
+
+let newArrayRange = arrayRange.map(function(element){
+  if (element.includes(1,2,3)) {
+    let wordsArrayRange = element.toString();
+    return wordsArrayRange
+    //return wordsArrayRange.replaceAll("")
+  }
+  else { (element !== 3)
+    return element
+  }
+
+});
+console.log(newArrayRange);
 }
   beepBoop(5);
+  //
