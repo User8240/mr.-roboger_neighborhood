00ee61c92e784c412dff8fee8145ed2e7470721a
Grace Kostanich <fl5812597@gmail.com>
2022-06-10 16:49:48 -0700

    Add final commit before deadline; project incomplete

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,17 @@
 //Business Logic
-
 function beepBoop(number) {
   let arrayRange = []
   for (let i = 0; i <= number; i++) {
       arrayRange.push(i)
   }
-  console.log(arrayRange);
-//cut previous working function
-
 let newArrayRange = arrayRange.map(function(element){
-
-  if (arrayRange.includes(element)) {
     let wordsArrayRange = element.toString();
-    //let wordsArrayRange = element.split("1")
     let stringWordsArrayRange = wordsArrayRange.replace("1", " Beep! ");
-
-    let pattern = "Bleep!"
-    let test = stringWordsArrayRange.length
-    console.log(test)
-//------------Testing Section-----------------------------------------
-
-
-  
-
-
-
-//--------------------------------------------------------------------
     return stringWordsArrayRange
-    }
-  });
-console.log(newArrayRange);
-}
+    });
+    console.log(newArrayRange);
+  }
   beepBoop(21);
-  //
+
+
+  //UI logic
